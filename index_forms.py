430e46caa9169a230abd65007fc9b1112bb6e487
Mikhail <110565276+ViolettaKabachko@users.noreply.github.com>
2022-09-11 14:16:01 +0200

    Update index_forms.py

--- index_forms.py
+++ index_forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import InputRequired, NumberRange
 
 
 class Menu(FlaskForm):
-    width = IntegerField("Ширина поля", validators=[InputRequired(), NumberRange(min=3, max=40)])
-    height = IntegerField("Высота поля", validators=[InputRequired(), NumberRange(min=3, max=40)])
+    width = IntegerField("Ширина поля", validators=[InputRequired(), NumberRange(min=3, max=30)])
+    height = IntegerField("Высота поля", validators=[InputRequired(), NumberRange(min=3, max=30)])
